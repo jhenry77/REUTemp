242a3f7c1364c61c96643ec48f574f214cea7f23
Ryan Canales <rechannels01@gmail.com>
2023-06-01 08:44:34 -0400

    Fixed left hand tracking

--- Assets/Scripts/MyHandViz.cs
+++ Assets/Scripts/MyHandViz.cs
@@ -33,7 +33,8 @@ namespace UnityEngine.XR.Hands
 
         
 
-        protected void Awake(){
+        protected void Awake()
+        {
         #if ENABLE_INPUT_SYSTEM
             InputSystem.InputSystem.settings.SetInternalFeatureFlag("USE_OPTIMIZED_CONTROLS", true);
         #endif // ENABLE_INPUT_SYSTEM
@@ -62,8 +63,6 @@ namespace UnityEngine.XR.Hands
             m_Subsystem = s_SubsystemsReuse[0];
             
             m_Subsystem.updatedHands += OnUpdatedHands;
-
-            //
         }
 
         void LateUpdate()
@@ -84,6 +83,7 @@ namespace UnityEngine.XR.Hands
 
             bool leftHandTracked = subsystem.leftHand.isTracked;
             bool rightHandTracked = subsystem.rightHand.isTracked;
+            var originPose = new Pose(myOrigin.position, myOrigin.rotation); // create a pose for XR origin
 
             if (leftHandTracked)
             {
@@ -98,28 +98,29 @@ namespace UnityEngine.XR.Hands
                     
                     var parentPose = Pose.identity;
                     wrist.TryGetPose(out Pose wristpose);
-                    var localWrist = wristpose.GetTransformedBy(myOrigin);
-                    parentPose = wristpose;
-                    for (int jointIndex = fingerId.GetFrontJointID().ToIndex(); jointIndex <= jointIndexBack; ++jointIndex)
+                    parentPose = wristpose.GetTransformedBy(originPose);
+
+                    for (int jointIndex = fingerId.GetFrontJointID().ToIndex(); jointIndex <= jointIndexBack; jointIndex++)
                     {
                         var otherFingerId = (XRHandJointID)jointIndex;
                         var joint = m_Subsystem.leftHand.GetJoint(otherFingerId);
                         joint.TryGetPose(out var pose);
-                        var localPose = pose.GetTransformedBy(myOrigin);
                         
-                        
-                        Quaternion joint_rotation = Quaternion.Euler(new Vector3(0, 0, 45));
-                        //joint_rotation.Set(joint_rotation.x, -joint_rotation.y, -joint_rotation.z, joint_rotation.w);
-                        //Quaternion.Euler(new Vector3(pose.rotation.eulerAngles.z, -pose.rotation.eulerAngles.y, pose.rotation.eulerAngles.x));
-                       // Quaternion joint_rotation = pose.rotation;
-                        joint_rotation = Quaternion.Inverse(parentPose.rotation) * Quaternion.Euler(new Vector3(0, 0, localPose.rotation.eulerAngles.x));
+                        Quaternion joint_rotation = Quaternion.Inverse(parentPose.rotation) * pose.rotation; // for default hand
+
+                        // We need to swap the XR hand X and Z axes for the robot hand
+                        joint_rotation.Set(joint_rotation.z, joint_rotation.y, joint_rotation.x, joint_rotation.w); 
+
                         switch(i){
-                            // case(2):
-                            // left_hand_joints[12].localRotation = joint_rotation;
-                            // break;
-                            // case(3):
-                            // left_hand_joints[13].localRotation =  joint_rotation;
-                            // break;
+                            // the thumb joints require different basis from other fingers
+                            case(2):
+                            joint_rotation.Set(joint_rotation.y, -joint_rotation.z, joint_rotation.x, joint_rotation.w);
+                            left_hand_joints[12].localRotation = joint_rotation;
+                            break;
+                            case(3):
+                            joint_rotation.Set(joint_rotation.y, -joint_rotation.z, joint_rotation.x, joint_rotation.w);
+                            left_hand_joints[13].localRotation =  joint_rotation;
+                            break;
                             case(6):
                             left_hand_joints[0].localRotation = joint_rotation;
                             break;
@@ -129,38 +130,38 @@ namespace UnityEngine.XR.Hands
                             case(8):
                             left_hand_joints[2].localRotation = joint_rotation;
                             break;
-                            // case(11):
-                            // left_hand_joints[3].localRotation = pose.rotation;
-                            // break;
-                            // case(12):
-                            // left_hand_joints[4].localRotation = pose.rotation;
-                            // break;
-                            // case(13):
-                            // left_hand_joints[5].localRotation = pose.rotation;
-                            // break;
-                            // case(16):
-                            // left_hand_joints[6].localRotation =  pose.rotation;
-                            // break;
-                            // case(17):
-                            // left_hand_joints[7].localRotation = pose.rotation;
-                            // break;
-                            // case(18):
-                            // left_hand_joints[8].localRotation = pose.rotation;
-                            // break;
-                            // case(21):
-                            // left_hand_joints[9].localRotation = pose.rotation;
-                            // break;
-                            // case(22):
-                            // left_hand_joints[10].localRotation = pose.rotation;
-                            // break;
-                            // case(23):
-                            // left_hand_joints[11].localRotation = pose.rotation;
-                            // break;
+                            case(11):
+                            left_hand_joints[3].localRotation = joint_rotation;
+                            break;
+                            case(12):
+                            left_hand_joints[4].localRotation = joint_rotation;
+                            break;
+                            case(13):
+                            left_hand_joints[5].localRotation = joint_rotation;
+                            break;
+                            case(16):
+                            left_hand_joints[9].localRotation =  joint_rotation;
+                            break;
+                            case(17):
+                            left_hand_joints[10].localRotation = joint_rotation;
+                            break;
+                            case(18):
+                            left_hand_joints[11].localRotation = joint_rotation;
+                            break;
+                            case(21):
+                            left_hand_joints[6].localRotation = joint_rotation;
+                            break;
+                            case(22):
+                            left_hand_joints[7].localRotation = joint_rotation;
+                            break;
+                            case(23):
+                            left_hand_joints[8].localRotation = joint_rotation;
+                            break;
                         }
                        
                         
                         parentPose = pose;
-                        print(otherFingerId.ToString() + ", " + i.ToString());
+                        // print(otherFingerId.ToString() + ", " + i.ToString());
                         i++;
                     }
                 }
